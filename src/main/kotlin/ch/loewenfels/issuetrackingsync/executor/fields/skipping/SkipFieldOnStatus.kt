20691a4b188b1b6ce5afea692c54624be614e4ed
Adrian Scherer <adrian.scherer@loewenfels.ch>
2020-01-28 15:10:55 +0100

    Suppress unchecked cast compiler warning

--- src/main/kotlin/ch/loewenfels/issuetrackingsync/executor/fields/skipping/SkipFieldOnStatus.kt
+++ src/main/kotlin/ch/loewenfels/issuetrackingsync/executor/fields/skipping/SkipFieldOnStatus.kt
@@ -12,6 +12,7 @@ open class SkipFieldOnStatus(fieldSkippingEvaluatorDefinition: FieldSkippingEval
         issue: Issue,
         fieldname: String
     ): Boolean {
+        @Suppress("UNCHECKED_CAST")
         val propIssue = issue.proprietaryTargetInstance as? T
         val status = propIssue?.let { issueClient.getState(it) } ?: ""
         return when (status) {
