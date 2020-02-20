3d4296a36f441b1b52a04ee607518e328c3d98bc
jdumon <joel.dumont@loewenfels.ch>
2020-02-20 10:31:59 +0100

    Added SyncFlowDefinitions as parameter to IssueFilter

--- src/main/kotlin/ch/loewenfels/issuetrackingsync/custom/UnclosedChangeWithRtcReferenceFilter.kt
+++ src/main/kotlin/ch/loewenfels/issuetrackingsync/custom/UnclosedChangeWithRtcReferenceFilter.kt
@@ -4,23 +4,32 @@ import ch.loewenfels.issuetrackingsync.Issue
 import ch.loewenfels.issuetrackingsync.executor.IssueFilter
 import ch.loewenfels.issuetrackingsync.syncclient.IssueTrackingClient
 import ch.loewenfels.issuetrackingsync.syncclient.jira.JiraClient
+import ch.loewenfels.issuetrackingsync.syncconfig.SyncFlowDefinition
 
 class UnclosedChangeWithRtcReferenceFilter :
     IssueFilter {
 
-    override fun test(client: IssueTrackingClient<out Any>, issue: Issue): Boolean {
+    override fun test(
+        client: IssueTrackingClient<out Any>,
+        issue: Issue,
+        syncFlowDefinition: SyncFlowDefinition
+    ): Boolean {
         return when (client) {
-            is JiraClient -> testUnclosedIssueWithRefToRtc(client, issue)
+            is JiraClient -> testUnclosedIssueWithRefToRtc(client, issue, syncFlowDefinition)
             else -> true
         }
     }
 
-    private fun testUnclosedIssueWithRefToRtc(client: JiraClient, issue: Issue): Boolean {
+    private fun testUnclosedIssueWithRefToRtc(
+        client: JiraClient,
+        issue: Issue,
+        syncFlowDefinition: SyncFlowDefinition
+    ): Boolean {
         val internalIssue = client.getProprietaryIssue(issue.key) as com.atlassian.jira.rest.client.api.domain.Issue
         val status = client.getValue(internalIssue, "status.name")
         val issueType = client.getValue(internalIssue, "issueType.name")
         val issueReference = client.getValue(
-            internalIssue, "customfield_12440"
+            internalIssue, syncFlowDefinition.writeBackFieldMappingDefinition!!.targetName
         )
         return UnclosedChangeFilter().getAllowedJiraIssueTypes().contains(status) //
                 && isAllowedIssueType(issueType, UnclosedChangeFilter().getAllowedJiraIssueTypes())//
--- src/main/kotlin/ch/loewenfels/issuetrackingsync/executor/IssueFilter.kt
+++ src/main/kotlin/ch/loewenfels/issuetrackingsync/executor/IssueFilter.kt
@@ -2,7 +2,11 @@ package ch.loewenfels.issuetrackingsync.executor
 
 import ch.loewenfels.issuetrackingsync.Issue
 import ch.loewenfels.issuetrackingsync.syncclient.IssueTrackingClient
+import ch.loewenfels.issuetrackingsync.syncconfig.SyncFlowDefinition
 
 interface IssueFilter {
-    fun test(client: IssueTrackingClient<out Any>, issue: Issue): Boolean
+    fun test(
+        client: IssueTrackingClient<out Any>, issue: Issue,
+        syncFlowDefinition: SyncFlowDefinition
+    ): Boolean
 }
