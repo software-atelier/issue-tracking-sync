359a9e089f380c7101125104a2797b4bba03fcc7
jdumon <joel.dumont@loewenfels.ch>
2020-02-26 14:22:45 +0100

    changed Post to Get Request because endpoint is used for testing purpose

--- src/main/kotlin/ch/loewenfels/issuetrackingsync/controller/DebugController.kt
+++ src/main/kotlin/ch/loewenfels/issuetrackingsync/controller/DebugController.kt
@@ -6,7 +6,6 @@ import ch.loewenfels.issuetrackingsync.syncconfig.Settings
 import org.apache.commons.io.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
@@ -22,7 +21,7 @@ class DebugController(
     @Autowired
     lateinit var syncApplicationProperties: SyncApplicationProperties
 
-    @PostMapping("/manualTimetrackingSync")
+    @GetMapping("/manualTimetrackingSync")
     fun startManualTimetrackingSync() {
         issuePoller.checkForUpdatedIssues()
     }
