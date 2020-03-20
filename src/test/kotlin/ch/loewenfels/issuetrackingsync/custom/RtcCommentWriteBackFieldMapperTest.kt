73b9a73ff4cee9863425376329c2680b7712d53f
DWicki <wicki.dane@protonmail.com>
2020-03-20 15:11:28 +0100

    Add a custom field Mapper for syncing as writeBackFieldMapper the URL from Jira to RTC as a Comment

---
+++ src/test/kotlin/ch/loewenfels/issuetrackingsync/custom/RtcCommentWriteBackFieldMapperTest.kt
@@ -0,0 +1,21 @@
+package ch.loewenfels.issuetrackingsync.custom
+
+import ch.loewenfels.issuetrackingsync.any
+import ch.loewenfels.issuetrackingsync.syncclient.IssueTrackingClient
+import junit.framework.Assert.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+
+internal class RtcCommentWriteBackFieldMapperTest {
+
+    @Test
+    fun getValue() {
+        val testee = RtcCommentWriteBackFieldMapper()
+        val issueTrackingClient = Mockito.mock(IssueTrackingClient::class.java) as IssueTrackingClient<String>
+        val url = "https://www.example.com/"
+        val fieldname = "Remote Link:"
+        Mockito.`when`(issueTrackingClient.getIssueUrl(any())).thenReturn(url)
+        val result = testee.getValue("", fieldname, issueTrackingClient)
+        assertEquals("$fieldname $url", result)
+    }
+}
