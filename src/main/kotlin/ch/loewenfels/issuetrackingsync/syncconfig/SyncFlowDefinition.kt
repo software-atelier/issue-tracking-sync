fa8a0e65e22c83d3674954a3ba2eadc1a57a8eb5
jdumon <joel.dumont@loewenfels.ch>
2020-05-06 08:36:30 +0200

    OS-82 Json Fix Single Value in Arrays
    - Settings-File could not been parsed, due to the fact that arrays with single value
    should start with curly-braces.

--- src/main/kotlin/ch/loewenfels/issuetrackingsync/syncconfig/SyncFlowDefinition.kt
+++ src/main/kotlin/ch/loewenfels/issuetrackingsync/syncconfig/SyncFlowDefinition.kt
@@ -1,5 +1,7 @@
 package ch.loewenfels.issuetrackingsync.syncconfig
 
+import com.fasterxml.jackson.annotation.JsonFormat
+
 data class SyncFlowDefinition(
     var name: String = "",
     var source: TrackingApplicationName = "",
@@ -7,6 +9,7 @@ data class SyncFlowDefinition(
     var filterClassname: String? = null,
     var defaultsForNewIssue: DefaultsForNewIssue? = null,
     var keyFieldMappingDefinition: FieldMappingDefinition = FieldMappingDefinition(),
+    @JsonFormat(with = [JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY])
     var writeBackFieldMappingDefinition: List<FieldMappingDefinition> = emptyList(),
     var actions: MutableList<String> = mutableListOf()
 )
